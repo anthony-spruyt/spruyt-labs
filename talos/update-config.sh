644bd001087dd1f4a0eb420a23e02477ca207e55
Anthony Spruyt <aspruyt@hotmail.co.uk>
2025-08-16 09:35:25 +0000

    chore: ip6

--- talos/update-config.sh
+++ talos/update-config.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 source "$(dirname "${BASH_SOURCE[0]}")/config.sh"
 
-talosctl apply-config -n ${C1_IP4} -f /workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${C1_HOST}.yaml -m=auto  # reboot | auto
-talosctl apply-config -n ${C2_IP4} -f /workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${C2_HOST}.yaml -m=auto  # reboot | auto
-talosctl apply-config -n ${C3_IP4} -f /workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${C3_HOST}.yaml -m=auto  # reboot | auto
+talosctl apply-config -n ${C1_IP4} -f /workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${C1_HOST}.yaml -m=reboot  # reboot | auto
+#talosctl apply-config -n ${C2_IP4} -f /workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${C2_HOST}.yaml -m=reboot  # reboot | auto
+#talosctl apply-config -n ${C3_IP4} -f /workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${C3_HOST}.yaml -m=reboot  # reboot | auto
