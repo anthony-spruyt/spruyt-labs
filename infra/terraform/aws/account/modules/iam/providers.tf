2f5a6d8ec153cb072cdcb16a0bf3a6834805c243
Anthony Spruyt <aspruyt@hotmail.co.uk>
2025-08-26 14:13:04 +0000

    fix: tf val

--- infra/terraform/aws/account/modules/iam/providers.tf
+++ infra/terraform/aws/account/modules/iam/providers.tf
@@ -1,8 +1,9 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.10"
-    }
-  }
+variable "aws_region" {
+  type        = string
+  description = "AWS region for resources"
+  default     = "ap-southeast-2" # Sydney
+}
+
+provider "aws" {
+  region = var.aws_region
 }
