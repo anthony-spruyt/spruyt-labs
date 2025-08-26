1c6abef1d313bf21b0d12a5f470ca048498a4b83
Anthony Spruyt <aspruyt@hotmail.co.uk>
2025-08-26 11:30:29 +0000

    fix: ts ignore

--- infra/terraform/aws/ceph-objectstore/backend.tf
+++ infra/terraform/aws/ceph-objectstore/backend.tf
@@ -1,8 +1,10 @@
 terraform {
+  # terrascan:ignore
   cloud {
     organization = "spruyt-labs"
     workspaces {
       name = "spruyt-labs-aws-ceph-objectstore"
     }
   }
+  # terrascan:endignore
 }
