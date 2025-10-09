5169a1cdc6b0b95e347500e01060b78fd5aec497
Anthony Spruyt <aspruyt@hotmail.co.uk>
2025-10-09 10:33:38 +0000

    chore: bgp 1

--- .taskfiles/talos/scripts/apply-config.sh
+++ .taskfiles/talos/scripts/apply-config.sh
@@ -7,5 +7,5 @@ talosctl apply-config -n "${C1_IP4}" -f "/workspaces/spruyt-labs/talos/clusterco
 talosctl apply-config -n "${C2_IP4}" -f "/workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${C2_HOST}.yaml" -m=auto  # reboot | auto
 talosctl apply-config -n "${C3_IP4}" -f "/workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${C3_HOST}.yaml" -m=auto  # reboot | auto
 talosctl apply-config -n "${W1_IP4}" -f "/workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${W1_HOST}.yaml" -m=auto  # reboot | auto
-# talosctl apply-config -n "${W2_IP4}" -f "/workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${W2_HOST}.yaml" -m=auto  # reboot | auto
-# talosctl apply-config -n "${W3_IP4}" -f "/workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${W3_HOST}.yaml" -m=auto  # reboot | auto
+talosctl apply-config -n "${W2_IP4}" -f "/workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${W2_HOST}.yaml" -m=auto  # reboot | auto
+talosctl apply-config -n "${W3_IP4}" -f "/workspaces/spruyt-labs/talos/clusterconfig/${CLUSTER_NAME}-${W3_HOST}.yaml" -m=auto  # reboot | auto
